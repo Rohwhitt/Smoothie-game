25a33e6018134aceee58f1753f3a15dce689f59d
Rohwhitt <70130968+Rohwhitt@users.noreply.github.com>
2021-03-03 16:01:51 +0800

    Recipe and Playing Features
    -playing boolean to stop clicking on ingredients
    - recipe display and hide all worked with timed feature

--- main.js
+++ main.js
@@ -1,41 +1,85 @@
 "use strict";
 //create a game that allows a person to make a set of smoothies and scores them on their performance
 // create all the query selectors
-const fruit = document.querySelectorAll(".btn-fruit");
+const fruit = document.querySelectorAll(".btn-ingredient");
 const submit = document.querySelector(".btn-submit");
+const check = document.querySelector(".btn");
+const ingredientsModal = document.querySelector(".recipe-ingredients");
+
 //create smoothie names and the fruits needed and store as array
 const bananaSmoothie = ["banana", "milk", "yohgurt"];
-const bigBerry = ["blueBerry", "raspberry", "strawberry"];
+const bigBerry = ["blueberry", "raspberry", "strawberry"];
 const fiestyIcy = ["mango", "pinapple", "ice"];
-const recipes = [bananaSmoothie, bigBerry, fiestyIcy];
+const summerSmoothie = ["apple", "orange", "pineapple", "mango"];
+const dairyDose = ["milk", "yohgurt", "banana", "chocolate"];
+const funNFruity = ["mango", "pineapple", "apple", "orange", "ice"];
+const tropical = [
+  "apple",
+  "orange",
+  "mango",
+  "Pineapple",
+  "ice",
+  "strawberry",
+  "raspberry",
+];
+const recipes = [
+  bananaSmoothie,
+  bigBerry,
+  fiestyIcy,
+  summerSmoothie,
+  dairyDose,
+  funNFruity,
+  tropical,
+];
 
-//create score variables.
+//create values.
 let totalScore = 0;
 let currentScore = 0;
+let playing = true;
+let ingredientAmount = 0;
 
-//display the smoothie name and all the ingredients for 3s
+let recipeNumber = 0;
 
-//hide recipe
+//display the smoothie name and all the ingredients for 3s then hide
+const displayRecipe = function () {
+  ingredientsModal.textContent = recipes[recipeNumber];
+  setTimeout(function () {
+    ingredientsModal.classList.add("hidden");
+  }, 3000);
+};
+displayRecipe();
 
 //function which checks what fruit clicked on and checks if its in the recipe array
 for (let i = 0; i < fruit.length; i++) {
   fruit[i].addEventListener("click", function () {
-    let fruitName = fruit[i].textContent;
-    if (recipes[0].includes(fruitName)) {
-      currentScore++;
-      console.log(currentScore, fruitName);
-    } else {
-      currentScore--;
-      console.log(currentScore, fruitName);
+    if (playing) {
+      let fruitName = fruit[i].textContent;
+      document.querySelector(fruitName).textContent = "test";
+      // add to the amount to ingredientAmount
+      ingredientAmount++;
+      if (recipes[recipeNumber].includes(fruitName)) {
+        currentScore++;
+        console.log(currentScore, fruitName, ingredientAmount);
+      } else {
+        currentScore--;
+        console.log(currentScore, fruitName, ingredientAmount);
+      }
     }
   });
 }
 
+const inAmount = function () {};
 //on click submit add current score to total score and move to next recipe
 submit.addEventListener("click", function () {
   totalScore += currentScore;
   currentScore = 0;
-  console.log(totalScore);
+  recipeNumber++;
+  ingredientsModal.classList.toggle("hidden");
+  displayRecipe();
+  if (recipeNumber >= recipes.length) {
+    playing = false;
+  }
+  console.log(totalScore, recipeNumber);
 });
 //load new recipe
 //reset current score and smoothie
