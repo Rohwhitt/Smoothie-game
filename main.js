b80307b05ccbf985b7eed7eac7af26253861633b
Rohwhitt <70130968+Rohwhitt@users.noreply.github.com>
2021-03-23 17:36:48 +0800

    Color Change
    - colors are now mixed using an overlay of divs with 0.1 opacity.
    - finds the current opacity of .smoothie and adds 0.1 if under 0.35
    - works with remake

--- main.js
+++ main.js
@@ -6,23 +6,42 @@ const submit = document.querySelector(".btn-submit");
 const check = document.querySelector(".btn");
 const ingredientsModal = document.querySelector(".recipe-ingredients");
 const remake = document.querySelector(".remake");
-
+const bgColor = document.querySelectorAll(".smoothie");
 //create smoothie names and the fruits needed and store as array
-const bananaSmoothie = ["banana", 1, "milk", 3, "yohgurt", 1];
-const bigBerry = ["blueberry", "raspberry", "strawberry"];
-const fiestyIcy = ["mango", "pinapple", "ice"];
-const summerSmoothie = ["apple", "orange", "pineapple", "mango"];
-const dairyDose = ["milk", "yohgurt", "banana", "chocolate"];
-const funNFruity = ["mango", "pineapple", "apple", "orange", "ice"];
-const tropical = [
-  "apple",
-  "orange",
+const bananaSmoothie = ["banana", 2, "milk", 3, "yohgurt", 1];
+const bigBerry = ["blueberry", 2, "raspberry", 2, "strawberry", 2];
+const fiestyIcy = ["mango", 3, "pinapple", 2, "ice", 1];
+const summerSmoothie = ["apple", 3, "orange", 2, "pineapple", 1, "mango", 2];
+const dairyDose = ["milk", 3, "yohgurt", 2, "banana", 1, "chocolate", 2];
+const funNFruity = [
   "mango",
-  "Pineapple",
+  2,
+  "pineapple",
+  1,
+  "apple",
+  2,
+  "orange",
+  2,
   "ice",
-  "strawberry",
-  "raspberry",
+  1,
 ];
+const extremeTropical = [
+  "apple",
+  4,
+  "orange",
+  4,
+  "mango",
+  3,
+  "Pineapple",
+  3,
+  "ice",
+  2,
+  "strawberry",
+  1,
+  "raspberry",
+  1,
+];
+//max score = 60. 1st 3 = 6. 2nd 3 = 8, last = 18
 const recipes = [
   bananaSmoothie,
   bigBerry,
@@ -30,9 +49,10 @@ const recipes = [
   summerSmoothie,
   dairyDose,
   funNFruity,
-  tropical,
+  extremeTropical,
 ];
-let ingredientAmount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// calculated using ingredientBtn[i].id as position in array
+let ingrAmountAdded = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 //if recipe includes ingredient and ingredient amount[i] = ?
 
 //const amountTest = bananaSmoothie[0 + 1];
@@ -52,23 +72,49 @@ const displayRecipe = function () {
   }, 3000);
 };
 displayRecipe();
+/*
+const colorChanger = function (opacity) {
+  for (let i = 0; i < bgColor.length; i++) {
+    //changes opacity based off number of clicks
+    bgColor[i].style.opacity = opacity;
+    console.log(opacity);
+  }
+};
+
+*/
+//one possible way is to use the text content to add that class. have all the divs premade
+//another way is to remove the hidden class from divs.
 
 //function which checks what fruit clicked on and checks if its in the recipe array
 for (let i = 0; i < ingredientBtn.length; i++) {
   ingredientBtn[i].addEventListener("click", function () {
     if (playing) {
-      //get ingredient id and store in variable
-      let ingredientNumber = ingredientBtn[i].id;
-      ingredientAmount[ingredientNumber]++;
-      //
+      //get current opacity value
+      const elementStyle = getComputedStyle(bgColor[i]);
+      let opacityValue = new Number(elementStyle.opacity);
+      // check opacity value and add opacity (color changer)
+      if (opacityValue <= 0.35) {
+        opacityValue += 0.1;
+        bgColor[i].style.opacity = opacityValue;
+        console.log(opacityValue);
+      } else {
+        console.log("no");
+      }
+      bgColor[i].classList.remove("hidden");
 
+      //get ingredient id and store in array
+      let ingredientNumber = ingredientBtn[i].id;
+      //add 1 to that position in the array
+      ingrAmountAdded[ingredientNumber]++;
+      //
       let fruitName = ingredientBtn[i].textContent;
       let currentRecipe = recipes[recipeNumber];
+      //checks postion in recipie based off fruits name and then +1
       let ingrQ = currentRecipe.indexOf(fruitName) + 1;
-      // add to the amount to ingredientAmount
+      // add to the amount to ingrAmountAdded
       if (
         currentRecipe.includes(fruitName) &&
-        ingredientAmount[ingredientNumber] <= currentRecipe[ingrQ]
+        ingrAmountAdded[ingredientNumber] <= currentRecipe[ingrQ]
       ) {
         currentScore++;
         console.log(currentScore, fruitName);
@@ -97,4 +143,9 @@ submit.addEventListener("click", function () {
 //reset current smoothie
 remake.addEventListener("click", function () {
   currentScore = 0;
+  ingrAmountAdded = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (let i = 0; i < bgColor.length; i++) {
+    bgColor[i].classList.add("hidden");
+    bgColor[i].style.opacity = 0.1;
+  }
 });
